f5fc15289e94472fefbb0f9cb85a879e3341ee6a
murphyy - rex - R3X - Rl3X <88536200+Rmurp3hyyX@users.noreply.github.com>
2021-08-25 00:13:53 +0300

    Update Pcbilgileriniöğrenme.py

--- Pcbilgileriniöğrenme.py
+++ Pcbilgileriniöğrenme.py
@@ -1,7 +1,7 @@
 #coding=utf-8
 import os
 d = os.uname()
-print(d)
+
 print("\33[1;31mBİLGİSAYAR BİLGİLERİNİ ÖĞRENME ARACI KODED BAY MURPHYY")
 print("""\33[1;34m
 İşletim sitemi adı:\33[1;33m {} {}
